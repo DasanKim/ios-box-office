19d97c13676cf267e53d3485ba8fe8700a10b903
Whales <Whales.w.Jin@gmail.com>
2023-07-26 11:21:01 +0900

    🩹chore: decodeJSON 메서드의 제네릭타입명 Value로 수정

--- BoxOffice/Model/DecodingManager.swift
+++ BoxOffice/Model/DecodingManager.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 struct DecodingManager {
-    func decodeJSON<T: Decodable>(fileName: String) throws -> T {
+    func decodeJSON<Value: Decodable>(fileName: String) throws -> Value {
         let decoder = JSONDecoder()
         
         guard let dataAsset = NSDataAsset(name: fileName) else {
             throw DataError.notFoundAsset
         }
         
-        guard let decodedData: T = try? decoder.decode(T.self, from: dataAsset.data) else {
+        guard let decodedData: Value = try? decoder.decode(Value.self, from: dataAsset.data) else {
             throw DataError.failedDecoding
         }
         
