5cd8f24c1f7ed5200fcb571369710075e362ee25
Whales <Whales.w.Jin@gmail.com>
2023-07-27 21:41:19 +0900

    🩹chore: 각각의 관련 타입들 BoxOffice, Movie 파일로 병합

--- BoxOffice/Model/BoxOffice.swift
+++ BoxOffice/Model/BoxOffice.swift
@@ -8,3 +8,57 @@
 struct BoxOffice: Decodable {
     let boxOfficeResult: DailyBoxOfficeResult
 }
+
+struct DailyBoxOfficeResult: Decodable {
+    let boxOfficeType: String
+    let showRange: String
+    let dailyBoxOfficeList: [BoxOfficeData]
+    
+    private enum CodingKeys: String, CodingKey {
+        case boxOfficeType = "boxofficeType"
+        case showRange
+        case dailyBoxOfficeList
+    }
+}
+
+struct BoxOfficeData: Decodable {
+    let number: String
+    let rank: String
+    let rankIntensity: String
+    let rankOldOrNew: String
+    let movieCode: String
+    let movieName: String
+    let openingDate: String
+    let salesAmount: String
+    let salesShare: String
+    let salesIntensity: String
+    let salesChangeRatio: String
+    let salesAccumulate: String
+    let audienceCount: String
+    let audienceIntensity: String
+    let audienceChangeRatio: String
+    let audienceAccumulate: String
+    let screenCount: String
+    let showCount: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case number = "rnum"
+        case rank
+        case rankIntensity = "rankInten"
+        case rankOldOrNew = "rankOldAndNew"
+        case movieCode = "movieCd"
+        case movieName = "movieNm"
+        case openingDate = "openDt"
+        case salesAmount = "salesAmt"
+        case salesShare
+        case salesIntensity = "salesInten"
+        case salesChangeRatio = "salesChange"
+        case salesAccumulate = "salesAcc"
+        case audienceCount = "audiCnt"
+        case audienceIntensity = "audiInten"
+        case audienceChangeRatio = "audiChange"
+        case audienceAccumulate = "audiAcc"
+        case screenCount = "scrnCnt"
+        case showCount = "showCnt"
+    }
+}
