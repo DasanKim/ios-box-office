7bd7cd1dded82406b19e1499c4c68e01bb5a2f2f
DasanKim <tempzipp@gmail.com>
2023-07-31 22:10:26 +0900

    🩹chore: 네이밍 수정 및 URL+ 파일 그룹 이동

--- BoxOffice/Utility/URL+.swift
+++ BoxOffice/Utility/URL+.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 extension URL {
-    init?(_ baseURL: String, _ path: String, _ query: [URLQueryItem]) {
+    init?(_ baseURL: String, _ path: String, _ queries: [URLQueryItem]) {
         var urlComponents = URLComponents(string: baseURL)
         urlComponents?.path = path
-        urlComponents?.queryItems = query
+        urlComponents?.queryItems = queries
 
         guard let url = urlComponents?.url else {
             return nil
