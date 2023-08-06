f40bf304a82d03c2c6dcdfc529467e9095c6c26b
Whales <Whales.w.Jin@gmail.com>
2023-08-07 00:49:36 +0900

    ✨feat: 천단위 구분 기호를 위한 CountFormatter 생성

---
+++ BoxOffice/Utility/CountFormatter.swift
@@ -0,0 +1,16 @@
+//
+//  CountFormatter.swift
+//  BoxOffice
+//
+//  Created by Dasan & Whales on 2023/08/07.
+//
+
+import Foundation
+
+struct CountFormatter {
+    static var decimal: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter
+    }()
+}
