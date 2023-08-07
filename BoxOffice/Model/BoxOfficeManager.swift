486e3cdef85edbb7de9e4f7ff01df5d68aaf6b45
Whales <Whales.w.Jin@gmail.com>
2023-08-08 00:44:53 +0900

    ♻️refactor: BoxOfficeManager 내 do-catch문 로직 개선

--- BoxOffice/Model/BoxOfficeManager.swift
+++ BoxOffice/Model/BoxOfficeManager.swift
@@ -17,13 +17,17 @@ final class BoxOfficeManager {
         
         networkManager.fetchData(url: KobisOpenAPI.boxOffice(targetDate: targetDate.formatNoSeparator()).url) { (data, error) in
             do {
+                if let error = error {
+                    completionHandler(.failure(error))
+                    
+                    return
+                }
+                
                 if let data = data {
                     let decodedData = try DecodingManager.decodeJSON(type: BoxOffice.self, data: data)
                     print(decodedData)
                     let boxOfficeItems = decodedData.boxOfficeResult.dailyBoxOfficeList
                     completionHandler(.success(boxOfficeItems))
-                } else if let error = error {
-                    completionHandler(.failure(error))
                 }
             } catch {
                 completionHandler(.failure(error))
