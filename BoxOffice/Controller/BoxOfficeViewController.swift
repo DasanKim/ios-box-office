ea49f41ce75ce9bcf31c7b4ed8aeaab62bda60dd
Whales <Whales.w.Jin@gmail.com>
2023-08-19 23:23:00 +0900

    ✨feat: GridCell 화면 구현을 위한 메서드 추가 및 전반적인 수정

--- BoxOffice/Controller/BoxOfficeViewController.swift
+++ BoxOffice/Controller/BoxOfficeViewController.swift
@@ -12,6 +12,11 @@ private enum Section: Hashable {
     case main
 }
 
+private enum CollectionViewStyle {
+    case list
+    case grid
+}
+
 final class BoxOfficeViewController: UIViewController {
     private let yesterday = TargetDate(dayFromNow: -1)
     private var items = [BoxOfficeData]()
@@ -19,16 +24,18 @@ final class BoxOfficeViewController: UIViewController {
     private var collectionView: UICollectionView! = nil
     private var dataSource: UICollectionViewDiffableDataSource<Section, BoxOfficeData>! = nil
     private let activityIndicatorView = UIActivityIndicatorView()
+    private var collectionViewStyle: CollectionViewStyle = .list
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureHierarchy()
+        configureHierarchy(style: collectionViewStyle)
         configureActivityIndicatorView()
         startActivityIndicator()
         loadData()
-        configureDataSource()
+        configureDataSource(style: collectionViewStyle)
         configureNavigationItem(title: yesterday.formattedWithHyphen())
+        configureToolbar()
         
         collectionView.delegate = self
     }
@@ -63,8 +70,13 @@ extension BoxOfficeViewController {
         }
     }
     
-    private func configureHierarchy() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createListLayout())
+    private func configureHierarchy(style: CollectionViewStyle) {
+        if style == .list {
+            collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createListLayout())
+        } else {
+            collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createGridLayout())
+        }
+        
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
         view.addSubview(collectionView)
@@ -97,15 +109,28 @@ extension BoxOfficeViewController {
         return layout
     }
     
-    private func configureDataSource() {
-        let cellRegistration = UICollectionView.CellRegistration<BoxOfficeCell, BoxOfficeData> { (cell, indexPath, item) in
-            let rankIntensityText = self.configureRankIntensity(with: item)
-            cell.updateLabel(with: item, rankIntensityText)
-        }
-        
-        dataSource = UICollectionViewDiffableDataSource<Section, BoxOfficeData>(collectionView: collectionView) {
-            (collectionView: UICollectionView, indexPath: IndexPath, identifier: BoxOfficeData) -> UICollectionViewListCell? in
-            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+    private func configureDataSource(style: CollectionViewStyle) {
+        if style == .list {
+            let cellRegistration = UICollectionView.CellRegistration<BoxOfficeCell, BoxOfficeData> { (cell, indexPath, item) in
+                let rankIntensityText = self.configureRankIntensity(with: item)
+                cell.updateLabel(with: item, rankIntensityText)
+            }
+            
+            dataSource = UICollectionViewDiffableDataSource<Section, BoxOfficeData>(collectionView: collectionView) {
+                (collectionView: UICollectionView, indexPath: IndexPath, identifier: BoxOfficeData) -> UICollectionViewListCell? in
+                return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+            }
+            
+        } else {
+            let cellRegistration = UICollectionView.CellRegistration<BoxOfficeGridCell, BoxOfficeData> { (cell, indexPath, item) in
+                let rankIntensityText = self.configureRankIntensity(with: item)
+                cell.updateLabel(with: item, rankIntensityText)
+            }
+            
+            dataSource = UICollectionViewDiffableDataSource<Section, BoxOfficeData>(collectionView: collectionView) {
+                (collectionView: UICollectionView, indexPath: IndexPath, identifier: BoxOfficeData) -> UICollectionViewCell? in
+                return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+            }
         }
         
         applySnapshot()
@@ -147,6 +172,59 @@ extension BoxOfficeViewController {
     
     private func configureNavigationItem(title: String) {
         navigationItem.title = title
+    }
+    
+    private func configureToolbar() {
+        navigationController?.isToolbarHidden = false
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let changeViewModeButton = UIBarButtonItem(title: "화면 모드 변경", style: .plain, target: self, action: #selector(tappedChangeViewModeButton))
+        
+        var items = [UIBarButtonItem]()
+        
+        items.append(flexibleSpace)
+        items.append(changeViewModeButton)
+        items.append(flexibleSpace)
+        
+        toolbarItems = items
+    }
+    
+    @objc func tappedChangeViewModeButton() {
+        showAlertController(currentStyle: collectionViewStyle)
+    }
+    
+    private func showAlertController(currentStyle: CollectionViewStyle) {
+        var message: String {
+            switch currentStyle {
+            case .list :
+                return "아이콘"
+            case .grid :
+                return "리스트"
+            }
+        }
+        
+        let alert = UIAlertController(title: "화면모드변경", message: nil, preferredStyle: UIAlertController.Style.actionSheet)
+        
+        let changeAction = UIAlertAction(title: message, style: .default) { _ in
+            self.changeMode()
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        
+        alert.addAction(changeAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    private func changeMode() {
+        if collectionViewStyle == .list {
+            collectionViewStyle = .grid
+        } else {
+            collectionViewStyle = .list
+        }
+
+        configureHierarchy(style: collectionViewStyle)
+        configureDataSource(style: collectionViewStyle)
     }
 }
 
